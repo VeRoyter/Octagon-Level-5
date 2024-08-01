7ed94891bfce5947999dd1ab53f429e57ae4ba9c
VeRoyter <137034950+VeRoyter@users.noreply.github.com>
2024-08-01 17:36:59 +1000

    Add files via upload

---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,28 @@
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  props:{
+    id: {type: Number, required: true},
+  },
+  computed: {
+    destination() {
+      return sourceData.destinations.find(destination => destination.id === this.id)
+    }
+  },
+}
+</script>
+
+<template>
+  <section v-if="destination" class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img :src="`/images/${destination.image}`" :alt="destination.name">
+      <p>{{ destination.description }}</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
